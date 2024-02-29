7d07b40fe83b166177098c6fae0b20543b5da3c1
juan.pardogarcia <jmpardogarcia7310@icloud.com>
2024-02-29 18:17:16 +0100

    Ahora analiza la respuesta y crea reclamaciones. Se agregaron muchas pruebas (que aún fallan) para controles de seguridad adicionales.

---
+++ Tests/SOLPYME.Saml2.Tests/Saml2RequestBaseTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace SolPyme.Saml2.Tests
+{
+    class ConcreteSaml2Request : Saml2RequestBase
+    {
+        public XElement ToXElement()
+        {
+            var x = new XElement("Foo");
+            x.Add(ToXNodes());
+            return x;
+        }
+    }
+
+    [TestClass]
+    public class Saml2RequestBaseTests
+    {
+        [TestMethod]
+        public void Saml2RequestBase_Id_IsUnique()
+        {
+            var r1 = new ConcreteSaml2Request();
+            var r2 = new ConcreteSaml2Request();
+
+            r1.Id.Should().NotBe(r2.Id);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_Id_IsValidXsId()
+        {
+            var id = new ConcreteSaml2Request().Id;
+
+            Regex.IsMatch(id, "[^:0-9][^:]*").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_Version()
+        {
+            new ConcreteSaml2Request().Version.Should().Be("2.0");
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_IssueInstant_Format()
+        {
+            Regex.IsMatch(new ConcreteSaml2Request().IssueInstant,
+                "20[0-9]{2}-((0[0-9])|(1[12]))-(([0-2][0-9])|(3[0-1]))T(([01][0-9])|(2[0-3]))(:[0-5][0-9]){2}Z")
+                .Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_IssueInstant_IsNow()
+        {
+            var issueInstant = new ConcreteSaml2Request().IssueInstant;
+
+            var parsed = DateTime.Parse(issueInstant).ToUniversalTime();
+
+            parsed.Should().BeCloseTo(DateTime.UtcNow, 1200);
+            parsed.Kind.Should().Be(DateTimeKind.Utc);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Id()
+        {
+            var r = new ConcreteSaml2Request();
+
+            r.ToXElement().Attribute("ID").Should().NotBeNull()
+                .And.Subject.Value.Should().Be(r.Id);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_IssueInstant()
+        {
+            var r = new ConcreteSaml2Request();
+            r.ToXElement().Attribute("IssueInstant").Should().NotBeNull()
+                .And.Subject.Value.Should().Be(r.IssueInstant);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Version()
+        {
+            var r = new ConcreteSaml2Request();
+            r.ToXElement().Attribute("Version").Should().NotBeNull()
+                .And.Subject.Value.Should().Be(r.Version);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Saml2PNamespacePrefix()
+        {
+            var r = new ConcreteSaml2Request();
+            r.ToXElement().GetPrefixOfNamespace(Saml2Namespaces.Saml2P).Should().Be("saml2p");
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Destination()
+        {
+            var uri = "http://idp.example.com/";
+            var r = new ConcreteSaml2Request() { DestinationUri = new Uri(uri) };
+
+            r.ToXElement().Attribute("Destination").Should().NotBeNull()
+                .And.Subject.Value.Should().Be(uri);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Issuer()
+        {
+            var uri = "http://sp.example.com/";
+            var r = new ConcreteSaml2Request() { Issuer = uri };
+
+            r.ToXElement().Element(Saml2Namespaces.Saml2 + "Issuer").Value.Should().Be(uri);
+        }
+
+        [TestMethod]
+        public void Saml2RequestBase_ToXNodes_Saml2NamespacePrefix()
+        {
+            var r = new ConcreteSaml2Request();
+            r.ToXElement().GetPrefixOfNamespace(Saml2Namespaces.Saml2Name).Should().Be("saml2");
+        }
+    }
+}
---
+++ Tests/SOLPYME.Saml2.Tests/Saml2ResponseTests.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Security.Cryptography.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using System.Security.Claims;
+
+
+namespace SolPyme.Saml2.Tests
+{
+    [TestClass]
+    public class Saml2ResponseTests
+    {
+        [TestMethod]
+        public void Saml2Response_Read_BasicParams()
+        {
+            string response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Response_Read_BasicParams"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Requester"" />
+                </saml2p:Status>
+            </saml2p:Response>";
+
+            var expected = new
+            {
+                Id = "Saml2Response_Read_BasicParams",
+                IssueInstant = new DateTime(2013, 01, 01, 0, 0, 0, DateTimeKind.Utc),
+                Status = Saml2StatusCode.Requester,
+                Issuer = (string)null
+            };
+
+            Saml2Response.Read(response).ShouldBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void Saml2Response_Read_ThrowsOnNonXml()
+        {
+            Action a = () => Saml2Response.Read("not xml");
+
+            a.ShouldThrow<XmlException>()
+                .WithMessage("Data at the root level is invalid. Line 1, position 1.");
+        }
+
+        [TestMethod]
+        public void Saml2Response_Read_ThrowsWrongRootNodeName()
+        {
+            Action a = () => Saml2Response.Read("<saml2p:NotResponse xmlns:saml2p=\"urn:oasis:names:tc:SAML:2.0:protocol\" />");
+
+            a.ShouldThrow<XmlException>()
+                .WithMessage("Expected a SAML2 assertion document");
+        }
+
+        [TestMethod]
+        public void Saml2Response_Read_ThrowsWrongRootNamespace()
+        {
+            Action a = () => Saml2Response.Read("<saml2p:Response xmlns:saml2p=\"something\" /> ");
+            a.ShouldThrow<XmlException>()
+                .WithMessage("Expected a SAML2 assertion document");
+        }
+
+        [TestMethod]
+        public void Saml2Response_Read_ThrowsOnWrongVersion()
+        {
+            Action a = () => Saml2Response.Read("<saml2p:Response xmlns:saml2p=\""
+                + Saml2Namespaces.Saml2P + "\" Version=\"wrong\" />");
+
+            a.ShouldThrow<XmlException>()
+                .WithMessage("Wrong or unsupported SAML2 version");
+
+        }
+
+        [TestMethod]
+        public void Saml2Response_Read_Issuer()
+        {
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Respons_Read_Issuer"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z""
+            Issuer = ""https://some.issuer.example.com"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Requester"" />
+                </saml2p:Status>
+            </saml2p:Response>";
+
+            Saml2Response.Read(response).Issuer.Should().Be("https://some.issuer.example.com");
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnMissingSignature()
+        {
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Response_Validates_FalseOnMissingSignature"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z""
+            Issuer = ""https://some.issuer.example.com"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Requester"" />
+                </saml2p:Status>
+            </saml2p:Response>";
+
+            Saml2Response.Read(response).Validate(null).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_TrueOnCorrectMessage()
+        {
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Response_Validate_TrueOnCorrectMessage"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z""
+            Issuer = ""https://some.issuer.example.com"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Requester"" />
+                </saml2p:Status>
+            </saml2p:Response>";
+
+            var signedResponse = SignXml(response);
+
+            Saml2Response.Read(signedResponse).Validate(testCert).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnTamperedMessage()
+        {
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Response_Validate_FalseOnTamperedMessage"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z""
+            Issuer = ""https://some.issuer.example.com"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Requester"" />
+                </saml2p:Status>
+            </saml2p:Response>";
+
+            var signedResponse = SignXml(response);
+
+            signedResponse = signedResponse.Replace("2013-01-01", "2013-01-02");
+
+            Saml2Response.Read(signedResponse).Validate(testCert).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Saml2Response_GetClaims_CreateIdentities()
+        {
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            ID = ""Saml2Response_GetClaims_CreateIdentity"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z""
+            Issuer = ""https://some.issuer.example.com"">
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                <saml2:Assertion xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                Version=""2.0"" ID=""Saml2Response_GetClaims_CreateIdentity_Assertion1""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>SomeUser</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                </saml2:Assertion>
+                <saml2:Assertion xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                Version=""2.0"" ID=""Saml2Response_GetClaims_CreateIdentity_Assertion2""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>SomeOtherUser</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                </saml2:Assertion>            
+            </saml2p:Response>";
+
+            var c1 = new ClaimsIdentity("Federation");
+            c1.AddClaim(new Claim(ClaimTypes.NameIdentifier, "SomeUser", null, "https://idp.example.com"));
+            var c2 = new ClaimsIdentity("Federation");
+            c2.AddClaim(new Claim(ClaimTypes.NameIdentifier, "SomeOtherUser", null, "https://idp.example.com"));
+
+            var expected = new ClaimsIdentity[] { c1, c2 };
+
+            Saml2Response.Read(SignXml(response)).GetClaims()
+                .ShouldBeEquivalentTo(expected, opt => opt.IgnoringCyclicReferences());
+        }
+
+        [TestMethod]
+        public void Saml2Response_GetClaims_ThrowsOnResponseNotValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_GetClaims_ThrowsOnWrongAudience()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_GetClaims_ThrowsOnExpired()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnInvalidInResponseTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnSecondInResponseTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnReplay()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Saml2Response_Validate_FalseOnIncorrectInReplyTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static readonly X509Certificate2 testCert = new X509Certificate2("Kentor.AuthServices.Tests.pfx");
+        private string SignXml(string xml)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xml);
+
+            var signedXml = new SignedXml(xmlDoc);
+
+            signedXml.SigningKey = (RSACryptoServiceProvider)testCert.PrivateKey;
+
+            var reference = new Reference();
+            reference.Uri = "";
+            reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
+
+            signedXml.AddReference(reference);
+            signedXml.ComputeSignature();
+
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(signedXml.GetXml(), true));
+
+            return xmlDoc.OuterXml;
+        }
+    }
+}
