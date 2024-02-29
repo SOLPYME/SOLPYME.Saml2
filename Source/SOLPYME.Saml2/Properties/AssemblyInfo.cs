ca53e9c0e8c0ed1864f9dff0be00bff1f13ff282
juan.pardogarcia <jmpardogarcia7310@icloud.com>
2024-02-29 12:04:30 +0100

    Configurar diccionario de análisis de código. Se agregó el primer embrión de Saml2AuthenticationModule

--- Source/SOLPYME.Saml2/Properties/AssemblyInfo.cs
+++ Source/SOLPYME.Saml2/Properties/AssemblyInfo.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SOLPYME.Saml2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Saml2 Authentication for ASP.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SOLPYME")]
 [assembly: AssemblyProduct("SOLPYME.Saml2")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright SOLPYME ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
